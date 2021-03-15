092abf615ea49303113ea15295d93b64ac809b12
Belen Medina Roldan <bmedinaroldan@gmail.com>
2021-03-15 20:01:59 -0300

    Se agrega CRUD de ofertas

--- site/src/controllers/salesController.js
+++ site/src/controllers/salesController.js
@@ -36,5 +36,71 @@ module.exports = {
         } catch (error) {
             res.render('error', {error});
         }
+    },
+
+    getSales: async (req, res) => {
+        try {            
+            let sales = await db.MovieSale.findAll({
+                where: {
+                    status: 1
+                },
+                include: {
+                    association: "movie"
+                }
+            }); 
+            
+            res.render('admin/sales', {
+                title: 'Ofertas vigentes',
+                css: '',
+                sales
+            })
+            
+        } catch (error) {
+            res.render('error', {error});
+        }
+    },
+
+    getSale: async (req, res) => {
+        try {            
+            let sale = await db.MovieSale.findOne({
+                where: {
+                    status: 1,
+                    id: +req.params.id
+                },
+                include: {
+                    association: "movie"
+                }
+            });
+            
+            res.render('admin/saleDetail', {
+                title: sale.movie.name,
+                css: '',
+                sale
+            })
+            
+        } catch (error) {
+            res.render('error', {error});
+        }
+    },
+
+    toCreateSale: (req, res) => {
+        res.render('admin/createSale');
+    },
+
+    createSale: async (req, res) => {
+        res.redirect('/sales/show');
+    },
+
+    editSale: (req, res) => {
+        res.render('admin/editSale');
+    },
+
+    updateSale: async (req, res) => {
+        res.redirect('/sales/show');
+    },
+
+    deleteSale: async (req, res) => {
+        res.redirect('/sales/show');
     }
+
 }
--- site/src/routes/salesRouter.js
+++ site/src/routes/salesRouter.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const {showSales} = require('../controllers/salesController');
+const {showSales,editSale,getSale,getSales,createSale,deleteSale,toCreateSale,updateSale} = require('../controllers/salesController');
 
 router.get('/', showSales);
+
+router.get('/show', getSales);
+router.get('/show/:id', getSale);
+router.get('/create', toCreateSale);
+router.post('/create', createSale);
+router.get('/edit/:id', editSale);
+router.put('/edit/:id', updateSale);
+router.delete('/create', deleteSale);
 
 module.exports = router;
