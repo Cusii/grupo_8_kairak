d59861dda97a5d356e0ba9f1a3522a52e74d879f
Belen Medina Roldan <bmedinaroldan@gmail.com>
2021-03-19 15:31:06 -0300

    Se actualizan modelos Rating y RatingMovieUser

--- site/src/database/models/Rating.js
+++ site/src/database/models/Rating.js
@@ -32,7 +32,7 @@ module.exports = (sequelize, DataTypes) => {
 			type: DataTypes.INTEGER,
 			allowNull: false
 		},
-		totalVote: {
+		totalVotes: {
 			type: DataTypes.INTEGER,
 			allowNull: false
 		},
