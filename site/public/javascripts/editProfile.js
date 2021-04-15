f833f0c4bc416d943ced0b12381b90069858a55a
Belen Medina Roldan <bmedinaroldan@gmail.com>
2021-04-15 07:35:25 -0300

    Se agrega previsualizacion de foto de usuario

---
+++ site/public/javascripts/editProfile.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", () => {    
+
+    document.getElementById("avatar").onchange = function(e) {        
+        let reader = new FileReader();
+      
+        reader.readAsDataURL(e.target.files[0]);
+      
+        reader.onload = function(){
+            let preview = document.getElementById('preview-img');
+            let image = document.createElement('img');
+      
+            image.src = reader.result;
+        
+            preview.innerHTML = '';
+            preview.innerHTML = `<img src="${image.src}" class=" img-logo-person img-fluid m-3 rounded mx-auto d-block " alt="logo " width="150px">`;
+            //preview.append(image);
+        };
+    }
+
+})
